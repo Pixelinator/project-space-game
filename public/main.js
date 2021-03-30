8b2e9e915dfe3d577e599bb58e53957cb5b8a892
Dominic Forster <forsterdominic2@gmail.com>
2021-03-30 13:37:09 +0200

    basic electron + react setup

---
+++ public/main.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  // Create the browser window.
+  let win = new BrowserWindow({ width: 800, height: 600 });
+
+  // and load the index.html of the app.
+  win.loadURL(`file://${path.join(__dirname, "../build/index.html")}`);
+}
+app.on("ready", createWindow);
