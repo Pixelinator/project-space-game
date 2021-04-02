f03a1305d35550813c2af5966882bce41a014a6e
Dominic Forster <forsterdominic2@gmail.com>
2021-04-02 18:12:21 +0200

    added orbitControls

--- src/Game/Game.js
+++ src/Game/Game.js
@@ -1,15 +1,14 @@
-import React, { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Loading } from "../Loading/Loading";
-import Bee from "../Asset/Bee";
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Loading } from '../Loading/Loading'
+import { CameraControls } from '../CameraControls/CameraControls'
+import Bee from '../Asset/Bee'
 
 export function Game() {
   return (
     <>
-      <Canvas
-        style={{ height: window.innerHeight, background: "#171717" }}
-        camera={{ position: [0, 0, 50], near: 0.01, far: 10000, fov: 35 }}
-      >
+      <Canvas style={{ height: window.innerHeight, background: '#171717' }}>
+        <CameraControls />
         <ambientLight intensity={0.5} />
         <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
         <pointLight position={[-10, -10, -10]} />
@@ -18,5 +17,5 @@ export function Game() {
         </Suspense>
       </Canvas>
     </>
-  );
+  )
 }
