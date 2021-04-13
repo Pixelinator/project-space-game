de7da93bc61131e1a148dcd574cd9669c59aa053
Chietcom <tyronety@web.de>
2021-04-13 18:02:30 +0200

    Tried adding a banana model

--- src/Playground/Playground.js
+++ src/Playground/Playground.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Canvas } from '@react-three/fiber'
+import { Loading } from '../Loading/Loading'
+import { CameraControls } from '../CameraControls/CameraControls'
+import Banana from '../Asset/Banana'
+import Bee from '../Asset/Bee'
 
 export function Playground() {
 return (
-    <div>
-        <h2>Settings</h2>
-    </div>
+    <Canvas style={{ height: window.innerHeight, background: '#999999'}}>
+        <CameraControls />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+        <pointLight position={[-10, -10, -10]} />
+        <Suspense fallback={null}>
+          <Banana/>
+        </Suspense>
+    </Canvas>
     );
 }
